eccdc2c6d7d0c06f2379ecfe8740d170849bb355
xttxccx <tatianacuoco@gmail.com>
2022-05-02 13:55:39 -0300

    armado header y banner

--- ttccProfolio/src/app/app.module.ts
+++ ttccProfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProComponent } from './components/logo-arg-pro/logo-arg-pro.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ ttccProfolio/src/app/components/logo-arg-pro/logo-arg-pro.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoArgProComponent } from './logo-arg-pro.component';
+
+describe('LogoArgProComponent', () => {
+  let component: LogoArgProComponent;
+  let fixture: ComponentFixture<LogoArgProComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoArgProComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoArgProComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
