b3977d288f7fd0b56dda56550f1a676efe180e50
xttxccx <tatianacuoco@gmail.com>
2022-05-28 19:40:46 -0300

    header

--- FrontEnd/src/app/components/header/header.component.ts
+++ FrontEnd/src/app/components/header/header.component.ts
@@ -1,6 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+
+  constructor(private router:Router) {}
+   
+  goToPage(pageName:string):void{
+      this.router.navigate([`/${pageName}`]);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
+/*@Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
@@ -12,4 +35,4 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-}
+}*/
